f6492dae04ab41d596d7b686432b2c7fdf44e338
Damindu <damindu.14@cse.mrt.ac.lk>
2019-01-11 00:02:43 +0530

    restricted view begun

---
+++ app/src/main/java/com/example/damindu/fish_project/Restri_content.java
@@ -0,0 +1,184 @@
+package com.example.damindu.fish_project;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+public class Restri_content extends AppCompatActivity {
+
+    TextView textView;
+    TextView textView_1;
+    TextView name;
+    CircularImageView imageView;
+    ImageView femaleImage;
+    ImageView global;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_content);
+
+        //content id tag
+        textView = (TextView) findViewById(R.id.prohibited_cont);
+        textView_1 = (TextView) findViewById(R.id.prohibited_cont_1);
+        name = (TextView) findViewById(R.id.pro_name);
+        imageView = (CircularImageView) findViewById(R.id.circularImageViewFish);
+        femaleImage = (ImageView) findViewById(R.id.image_prob);
+        global = (ImageView) findViewById(R.id.global);
+        visibiltyGone();
+
+        //key value extract
+        Intent intent = getIntent();
+        String key = intent.getStringExtra("key");
+
+        String nameText = "";
+        String content = "";
+        String content_1 = "";
+        switch (key) {
+
+            case "1":
+                 nameText = getString(R.string.pro_1_name);
+                content = getString(R.string.pro_1);
+                content_1 = getString(R.string.pro_1_1);
+                imageView.setImageResource(R.drawable.pro_1);
+                global.setImageResource(R.mipmap.global_1);
+                femaleImage.setImageResource(R.drawable.pro_1_1);
+                break;
+
+            case "2":
+                imageView.setImageResource(R.drawable.pro_2);
+                global.setImageResource(R.mipmap.global_2);
+                nameText = getString(R.string.pro_2_name);
+                content = getString(R.string.pro_2);
+                content_1 = getString(R.string.pro_2_1);
+                findViewById(R.id.table_2).setVisibility(View.VISIBLE);
+                break;
+
+            case "3":
+                imageView.setImageResource(R.drawable.pro_3);
+                global.setImageResource(R.mipmap.global_3);
+                nameText = getString(R.string.pro_3_name);
+                content = getString(R.string.pro_3);
+                content_1 = getString(R.string.pro_3_1);
+                findViewById(R.id.table_3).setVisibility(View.VISIBLE);
+                break;
+
+            case "4":
+                imageView.setImageResource(R.drawable.pro_4);
+                global.setImageResource(R.mipmap.global_4);
+                nameText = getString(R.string.pro_4_name);
+                content = getString(R.string.pro_4);
+                content_1 = getString(R.string.pro_4_1);
+                findViewById(R.id.table_4).setVisibility(View.VISIBLE);
+                break;
+
+            case "5":
+                imageView.setImageResource(R.drawable.pro_5);
+                global.setImageResource(R.mipmap.global_5);
+                nameText = getString(R.string.pro_5_name);
+                content = getString(R.string.pro_5);
+                content_1 = getString(R.string.pro_5_1);
+                break;
+
+            case "6":
+                imageView.setImageResource(R.drawable.pro_6);
+                global.setImageResource(R.mipmap.global_6);
+                nameText = getString(R.string.pro_6_name);
+                content = getString(R.string.pro_6);
+                content_1 = getString(R.string.pro_6_1);
+                findViewById(R.id.table_6).setVisibility(View.VISIBLE);
+                break;
+
+            case "7":
+                imageView.setImageResource(R.drawable.pro_7);
+                global.setImageResource(R.mipmap.global_7);
+                nameText = getString(R.string.pro_7_name);
+                content = getString(R.string.pro_7);
+                content_1 = getString(R.string.pro_7_1);
+                findViewById(R.id.table_4).setVisibility(View.VISIBLE);
+                break;
+
+            case "8":
+                imageView.setImageResource(R.drawable.pro_8);
+                global.setImageResource(R.mipmap.global_8);
+                nameText = getString(R.string.pro_8_name);
+                content = getString(R.string.pro_8);
+                content_1 = getString(R.string.pro_8_1);
+                findViewById(R.id.table_8).setVisibility(View.VISIBLE);
+                break;
+
+            case "9":
+                imageView.setImageResource(R.drawable.pro_9);
+                global.setImageResource(R.mipmap.global_9);
+                nameText = getString(R.string.pro_9_name);
+                content = getString(R.string.pro_9);
+                content_1 = getString(R.string.pro_9_1);
+                break;
+            case "10":
+                imageView.setImageResource(R.drawable.pro_10);
+                global.setImageResource(R.mipmap.global_10);
+                nameText = getString(R.string.pro_10_name);
+                content = getString(R.string.pro_10);
+                content_1 = getString(R.string.pro_10_1);
+                break;
+
+            case "11":
+                imageView.setImageResource(R.drawable.pro_11);
+                global.setImageResource(R.mipmap.global_11);
+                nameText = getString(R.string.pro_11_name);
+                content = getString(R.string.pro_11);
+                content_1 = getString(R.string.pro_11_1);
+                break;
+
+            case "12":
+                imageView.setImageResource(R.drawable.pro_12);
+                global.setImageResource(R.mipmap.global_12);
+                nameText = getString(R.string.pro_12_name);
+                content = getString(R.string.pro_12);
+                content_1 = getString(R.string.pro_12_1);
+                femaleImage.setImageResource(R.drawable.pro_12_1);
+                break;
+
+            case "13":
+                imageView.setImageResource(R.drawable.pro_13);
+                global.setImageResource(R.mipmap.global_13);
+                nameText = getString(R.string.pro_13_name);
+                content = getString(R.string.pro_13);
+                content_1 = getString(R.string.pro_13_1);
+                findViewById(R.id.table_13).setVisibility(View.VISIBLE);
+                break;
+
+            default:
+                break;
+        }
+
+        // set Text in TextView using fromHtml() method with version check
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            textView.setText(Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY));
+            textView_1.setText(Html.fromHtml(content_1, Html.FROM_HTML_MODE_LEGACY));
+            name.setText(Html.fromHtml(nameText, Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            textView.setText(Html.fromHtml(content));
+            textView_1.setText(Html.fromHtml(content_1));
+            name.setText(Html.fromHtml(nameText));
+        }
+    }
+
+    protected void visibiltyGone(){
+            findViewById(R.id.table_2).setVisibility(View.GONE);
+            findViewById(R.id.table_3).setVisibility(View.GONE);
+            findViewById(R.id.table_4).setVisibility(View.GONE);
+            findViewById(R.id.table_6).setVisibility(View.GONE);
+            findViewById(R.id.table_7).setVisibility(View.GONE);
+            findViewById(R.id.table_8).setVisibility(View.GONE);
+            findViewById(R.id.table_13).setVisibility(View.GONE);
+    }
+
+}
---
+++ app/src/main/java/com/example/damindu/fish_project/Restricted_button.java
@@ -0,0 +1,143 @@
+package com.example.damindu.fish_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class Restricted_button extends AppCompatActivity implements View.OnClickListener {
+
+    private final static int REQUEST_CODE_1 = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restricted_button);
+
+//        final LinearLayout restricted = findViewById(R.id.p_1);
+//        restricted.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Prohibited_button.this, Prob_content.class));
+//            }
+//        });
+
+        LinearLayout one = (LinearLayout) findViewById(R.id.p_1);
+        one.setOnClickListener(this); // calling onClick() method
+        LinearLayout two = (LinearLayout) findViewById(R.id.p_2);
+        two.setOnClickListener(this);
+        LinearLayout three = (LinearLayout) findViewById(R.id.p_3);
+        three.setOnClickListener(this);
+        LinearLayout four = (LinearLayout) findViewById(R.id.p_4);
+        four.setOnClickListener(this); // calling onClick() method
+        LinearLayout five = (LinearLayout) findViewById(R.id.p_5);
+        five.setOnClickListener(this);
+        LinearLayout six = (LinearLayout) findViewById(R.id.p_6);
+        six.setOnClickListener(this);
+        LinearLayout seven = (LinearLayout) findViewById(R.id.p_7);
+        seven.setOnClickListener(this); // calling onClick() method
+        LinearLayout eight = (LinearLayout) findViewById(R.id.p_8);
+        eight.setOnClickListener(this);
+        LinearLayout nine = (LinearLayout) findViewById(R.id.p_9);
+        nine.setOnClickListener(this);
+        LinearLayout ten = (LinearLayout) findViewById(R.id.p_10);
+        ten.setOnClickListener(this); // calling onClick() method
+        LinearLayout eleven = (LinearLayout) findViewById(R.id.p_11);
+        eleven.setOnClickListener(this);
+        LinearLayout twelve = (LinearLayout) findViewById(R.id.p_12);
+        twelve.setOnClickListener(this);
+        LinearLayout thirteen = (LinearLayout) findViewById(R.id.p_13);
+        thirteen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.p_1:
+                Intent intent = new Intent(Restricted_button.this, Prob_content.class);
+                intent.putExtra("key", "1");
+                startActivityForResult(intent, REQUEST_CODE_1);
+
+                break;
+
+            case R.id.p_2:
+                Intent intent_1 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_1.putExtra("key", "2");
+                startActivityForResult(intent_1, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_3:
+                Intent intent_2 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_2.putExtra("key", "3");
+                startActivityForResult(intent_2, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_4:
+                Intent intent_4 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_4.putExtra("key", "4");
+                startActivityForResult(intent_4, REQUEST_CODE_1);
+
+                break;
+
+            case R.id.p_5:
+                Intent intent_5 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_5.putExtra("key", "5");
+                startActivityForResult(intent_5, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_6:
+                Intent intent_6 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_6.putExtra("key", "6");
+                startActivityForResult(intent_6, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_7:
+                Intent intent_7 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_7.putExtra("key", "7");
+                startActivityForResult(intent_7, REQUEST_CODE_1);
+
+                break;
+
+            case R.id.p_8:
+                Intent intent_8 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_8.putExtra("key", "8");
+                startActivityForResult(intent_8, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_9:
+                Intent intent_9 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_9.putExtra("key", "9");
+                startActivityForResult(intent_9, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_10:
+                Intent intent_10 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_10.putExtra("key", "10");
+                startActivityForResult(intent_10, REQUEST_CODE_1);
+
+                break;
+
+            case R.id.p_11:
+                Intent intent_11 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_11.putExtra("key", "11");
+                startActivityForResult(intent_11, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_12:
+                Intent intent_12 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_12.putExtra("key", "12");
+                startActivityForResult(intent_12, REQUEST_CODE_1);
+                break;
+
+            case R.id.p_13:
+                Intent intent_13 = new Intent(Restricted_button.this, Prob_content.class);
+                intent_13.putExtra("key", "13");
+                startActivityForResult(intent_13, REQUEST_CODE_1);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
